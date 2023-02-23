3a79756835c9c11159ba19199fd4c2883b2362b5
webferrol <xurxo@webferrol.com>
2023-02-23 13:26:03 +0100

    Refactorización terminada

--- exercises/js/05-prompt.js
+++ exercises/js/05-prompt.js
@@ -1,18 +1,4 @@
 'use strict'
-let cifra1,
-    cifra2,
-    num1,
-    num2,
-    resultado
-cifra1 = prompt("Dame primer número")
-cifra2 = prompt("Dame segundo número")
-
-num1 = Number(cifra1)
-num2 = Number(cifra2)
-
-resultado = num1 + num2
-
-alert(num1 + '+' + num2 + '=' + resultado)
-
-
-
+let cifra1 = prompt("Dame primer número")
+let cifra2 = prompt("Dame segundo número")
+alert(`${cifra1} + ${cifra2} = ${Number(cifra1) + Number(cifra2)}`)
