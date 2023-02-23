b74ad8b68ef510eec9de3f3e276d136a60fd17d2
webferrol <xurxo@webferrol.com>
2023-02-23 13:02:49 +0100

    Ejercicio 5 sin refactorizar

---
+++ exercises/js/04-prompt.js
@@ -0,0 +1,3 @@
+let nombre // << undefinded [Declaración sin asignación] 
+nombre = prompt('Su nombre:') // << string 
+alert('Hola ' + nombre) // >> Ejemplo: Hola Pepito
---
+++ exercises/js/05-prompt.js
@@ -0,0 +1,18 @@
+'use strict'
+let cifra1,
+    cifra2,
+    num1,
+    num2,
+    resultado
+cifra1 = prompt("Dame primer número")
+cifra2 = prompt("Dame segundo número")
+
+num1 = Number(cifra1)
+num2 = Number(cifra2)
+
+resultado = num1 + num2
+
+alert(num1 + '+' + num2 + '=' + resultado)
+
+
+
